d10a494297acee71c4cb4aaf628db975cfc10bb2
andre <andremaivelasco@hotmail.com>
2025-02-12 19:24:32 -0300

    service e json server

---
+++ client/src/services/taskService.ts
@@ -0,0 +1,50 @@
+import { Task } from "@/models/task";
+import axios, { AxiosInstance } from "axios";
+
+export class CondutorClient{
+    private axiosClient : AxiosInstance;
+
+    constructor() {
+        this.axiosClient = axios.create({
+            baseURL: "http://localhost:3000",
+            headers: {"Content-type" : "application/json"}
+        });
+    }
+
+    public async findAll(): Promise<Task[]> {
+        try{
+            return (await this.axiosClient.get<Task[]>(`/tasks`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async cadastrar(tarefa: Task): Promise<string> {
+        try{
+            return (await this.axiosClient.post<string>(`/tasks`, tarefa)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async editar(id: number, tarefa: Task): Promise<string> {
+        try{
+            return (await this.axiosClient.put<string>(`/tasks/${id}`, tarefa)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async excluir(id: number): Promise<string> {
+        try{
+            return (await this.axiosClient.delete<string>(`/tasks/${id}`)).data;
+        }
+        catch(error : any){
+            return Promise.reject(error.response);
+        }
+    }
+}
+export default new CondutorClient();
