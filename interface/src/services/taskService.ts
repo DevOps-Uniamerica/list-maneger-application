a5fc319c482e03ecb864b1f8b1ed406135bfa030
andre <andremaivelasco@hotmail.com>
2025-02-12 20:53:18 -0300

    model e service atualizada

--- interface/src/services/taskService.ts
+++ interface/src/services/taskService.ts
@@ -1,4 +1,5 @@
 // src/services/taskService.js
+import type { Task } from "@/models/task";
 import axios from "axios";
 
 const API_URL = "http://localhost:3000/tasks";
@@ -14,15 +15,32 @@ export default {
     }
   },
 
-  async addTask(description : string) {
-    if (!description.trim()) return;
-
-    const task = { description };
+  async addTask(tarefa: Task) {
     try {
-      const response = await axios.post(API_URL, task);
+      const response = await axios.post(API_URL, tarefa);
       return response.data;
     } catch (error) {
       console.error("Erro ao adicionar tarefa", error);
+      return null;
+    }
+  },
+
+  async updateTask(id: number, tarefa: Task) {
+    try {
+      const response = await axios.put(API_URL + "/" + id, tarefa);
+      return response.data;
+    } catch (error) {
+      console.error("Erro ao atualizar tarefa", error);
+      return null;
+    }
+  },
+
+  async deleteTask(id: number) {
+    try {
+      const response = await axios.delete(API_URL + "/" + id);
+      return response.data;
+    } catch (error) {
+      console.error("Erro ao deletar tarefa", error);
       return null;
     }
   }
